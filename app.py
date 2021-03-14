498aff150150b355e8edf9ef31f713646d2bac43
Grant Rosson <grantrosson@gmail.com>
2021-03-14 22:23:48 +0200

    use jinja to template html, create style.css, move texts, reposition scroll

--- app.py
+++ app.py
@@ -1,85 +1,17 @@
-from flask import Flask, request
+from flask import Flask, url_for, render_template
 from poem_machine_crawl import gather
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-# TODO
-# make other texts available
-# 
-
 @app.route('/')
 def poem_machine(source="dickinson"):
+    title = "Dickinson's Poems"
     poem = gather(source)
+    return render_template('body.html', poem=poem, title=title)
     return '''
-<html>
-    <head>
-
-	<style>
-		body,html{{
-			padding: 0;
-			margin: 0;
-		}}
-		ul, li {{
-			padding: 0;
-			margin: 0;
-			list-style: none;
-			text-align: left;
-		}}
-
-
-                .ye {{
-		    padding: 5px 0;
-		    list-style: none;
-                    text-align: center;
-                    margin: 0px auto;
-                    margin-left: 0;
-                }}
-
-		.container {{
-		    width: 450px;
-		    height: 845px;
-		    line-height: 30px;
-		    border: 0px white;
-                    right: 200;
-		    overflow: Hidden;
-		    padding: 5px 0;
-		    margin: 20px auto;
-                    -webkit-touch-callout: none; /* iOS Safari */
-                    -webkit-user-select: none; /* Safari */
-                    -khtml-user-select: none; /* Konqueror HTML */
-                    -moz-user-select: none; /* Firefox */
-                    -ms-user-select: none; /* Internet Explorer/Edge */
-                    user-select: none; /* Non-prefixed version, currently
-                                  supported by Chrome and Opera */
-		}}
-	</style>
-    </head>
-
-    <body>
-        <br>
-        <div class="ye">
-        <ye>Poem-Machine is now processing: <u><i>Dickinson's Poems</i></u></ye>
-        </div>
-	<div class="container">
-		<ul>
-			<li>&nbsp</li>
-			<li>{poem}</li>
-			<li>&nbsp</li>
-		</ul>
-	</div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" type="text/javascript"></script>
-    <script type="text/javascript" src="static/js/scrollText/jQuery.scrollText.js"></script>
-    <script type="text/javascript">
-		$(function(){{
-			$(".container").scrollText({{
-				'duration': 8000
-			}});
-		}});
-	</script>    
-    </body>
-</html>
+    {% block body %}
+    {% endblock %}
     '''.format(poem=poem)
-
 
 
 @app.route('/<source>')
@@ -102,75 +34,10 @@ def poem_machine_name(source="dickinson"):
             break
 
     poem = gather(source)
-      
+    return render_template('body.html', poem=poem, title=title)
     return '''
-<html>
-    <head>
-
-	<style>
-		body,html{{
-			padding: 0;
-			margin: 0;
-		}}
-
-                ul, li {{
-			padding: 0;
-			margin: 0;
-			list-style: none;
-			text-align: left;
-		}}
-
-                .ye {{
-		    padding: 5px 0;
-		    list-style: none;
-                    text-align: center;
-                    margin: 0px auto;
-                    margin-left: 0;
-                }}
-
-		.container {{
-		    width: 450px;
-		    height: 845px;
-		    line-height: 30px;
-		    border: 0px white;
-                    right: 200;
-		    overflow: Hidden;
-		    padding: 5px 0;
-		    margin: 20px auto;
-                    -webkit-touch-callout: none; /* iOS Safari */
-                    -webkit-user-select: none; /* Safari */
-                    -khtml-user-select: none; /* Konqueror HTML */
-                    -moz-user-select: none; /* Firefox */
-                    -ms-user-select: none; /* Internet Explorer/Edge */
-                    user-select: none; /* Non-prefixed version, currently
-                                  supported by Chrome and Opera */
-		}}
-	</style>
-    </head>
-
-    <body>
-        <br>
-        <div class="ye">
-        <ye>Poem-Machine is now processing: <u><i>{title}</i></u></ye>
-        </div>
-        <div class="container">
-		<ul>
-			<li>&nbsp</li>
-			<li>{poem}</li>
-			<li>&nbsp</li>
-		</ul>
-	</div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" type="text/javascript"></script>
-    <script type="text/javascript" src="static/js/scrollText/jQuery.scrollText.js"></script>
-    <script type="text/javascript">
-		$(function(){{
-			$(".container").scrollText({{
-				'duration': 8000
-			}});
-		}});
-	</script>    
-    </body>
-</html>
+    {% block body %}
+    {% endblock %}
     '''.format(poem=poem, title=title)
 
 
--- poem_machine_crawl.py
+++ poem_machine_crawl.py
@@ -4,7 +4,7 @@ def gather(source):
     words_lower_limit = 4
     words_upper_limit = 10
 
-    corpus_path = "static/texts/{}.txt".format(source)
+    corpus_path = "texts/{}.txt".format(source)
 
    # open corpus from file and read into memory
     with open(corpus_path, 'rt') as corpus:
